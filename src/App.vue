0e147a37d9cf33c81b4ed008d5df89aec90ad418
Arthur Prudhomme <arthurprudhommemail06@gmail.com>
2023-01-23 08:10:02 +0100

    :bug: prbs de mise en ligne

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,41 @@
 <template>
   <nav>
-    <router-link to="/journal">Journal</router-link> |
-    <router-link to="/contact">Contact</router-link> |
-    <router-link to="/clavier">Clavier</router-link>
+    <router-link to="/journal"><h1>Journal</h1></router-link>
+    <router-link to="/contact"><h1>Contact</h1></router-link>
+    <router-link to="/clavier"><h1>Clavier</h1></router-link>
   </nav>
   <router-view/>
 </template>
 
 <style>
+body{
+  background-color: #121920;
+  color: white;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  border: white solid 5px;
+  height: 30vh;
+  margin: auto;
 }
 
 nav {
-  padding: 30px;
+  display: flex;
+  flex-direction: column;
+  padding-right: 50px;
 }
 
 nav a {
   font-weight: bold;
-  color: #2c3e50;
+  color: white;
+  border: white solid 3px;
+  width: 200px;
 }
 
 nav a.router-link-exact-active {
